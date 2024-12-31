4189fbd1396d795cca236c5fe26dc6dbf3416bd7
Sue Fisher <sfisher@meadowbrook-ma.org>
2024-12-31 12:14:36 -0500

    Add files via upload
    I am uploading files from my desktop. I'd downloaded them from p5.js and unzipped them first. They draw an ice cream cone and I'm just learning how to use github repositories.

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //scoop of ice cream - pink
+  fill(239, 136, 236);
+  ellipse(180,180,40,50);
+  //cone is brown
+  fill(76, 46, 4);
+  triangle(160,200,200,200,180,260);
+}
